df349ee69b1d522d01d66a442eb757f19d2b791c
Rob Pelance <rpelance@gmail.com>
2021-02-21 00:57:41 +0000

    Add about section, fix nav js

--- Resources/Javascript/script.js
+++ Resources/Javascript/script.js
@@ -1,29 +1,8 @@
 function mobileNav() {
-		let nav1 = document.getElementById("nav1");
-		if (nav1.className === "nav-item") {
-			nav1.className += " responsive";
-		} else {
-			nav1.className = "nav-item";
-		};
-		
-		let nav2 = document.getElementById("nav2");
-		if (nav2.className === "nav-item") {
-			nav2.className += " responsive";
-		} else {
-			nav2.className = "nav-item";
-		};
-		
-		let nav3 = document.getElementById("nav3");
-		if (nav3.className === "nav-item") {
-			nav3.className += " responsive";
-		} else {
-			nav3.className = "nav-item";
-		};
-		
-		let nav4 = document.getElementById("nav4");
-		if (nav4.className === "nav-item") {
-			nav4.className += " responsive";
-		} else {
-			nav4.className = "nav-item";
-		};
-	}
+	const elements = ['nav1', 'nav2', 'nav3', 'nav4', 'nav5'];
+	
+	elements.forEach(id => {
+	  const nav = document.getElementById(id);
+	  nav.classList.toggle('responsive');
+	});
+}
